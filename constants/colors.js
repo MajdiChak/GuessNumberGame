97a6b8231b0afe551f8413ddf899c8c35b1b38c1
majdi <majdich@gmail.com>
2021-10-26 14:13:25 +0200

    adding a Header components and colors constants

---
+++ constants/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+    primary: '#007bff',
+    secondary: '#8186A0',
+    success: '#03c04a',
+    danger: '#dc3545',
+    warning: '#ffc107',
+    info: '#17a2b8',
+    backgroundLight: '#F9F9FC',
+    backgroundDark: '#111111',
+    dark: '#aaaaaa',
+    light: '#eeeeee',
+    gray: '#aaaaaa',
+    white:"#EEEEEE",
+    black:"#222222"
+  }
+  
+  export default colors
