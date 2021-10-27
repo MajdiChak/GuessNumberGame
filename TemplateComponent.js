936d11940dd612793eb43f7803a9c2b2d161fe65
majdi <majdich@gmail.com>
2021-10-27 09:10:33 +0200

    adding input and output card

---
+++ TemplateComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import colors from '../constants/colors'
+
+function Test() {
+  return (
+    <View style={styles.test}>
+      <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  test: {},
+})
+
+export default Test
