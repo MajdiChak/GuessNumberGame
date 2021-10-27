07be9bfc0c6682e38ec3a12bbdee70ab7d18d5cf
majdi <majdich@gmail.com>
2021-10-27 20:15:58 +0200

    Final version

--- App.js
+++ App.js
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen';
-import GameOverScreen from './screens/GameOverScreen';
+import Header from './components/Header'
+import StartGameScreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen'
+import GameOverScreen from './screens/GameOverScreen'
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [guessRounds, setGuessRounds] = useState(0);
+  const [userNumber, setUserNumber] = useState()
+  const [guessRounds, setGuessRounds] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false)
 
   const configureNewGameHandler = () => {
-    setGuessRounds(0);
-    setUserNumber(null);
-  };
+    setGuessRounds(0)
+    setUserNumber(null)
+  }
 
-  const startGameHandler = selectedNumber => {
-    setUserNumber(selectedNumber);
-  };
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber)
+  }
 
-  const gameOverHandler = numOfRounds => {
-    setGuessRounds(numOfRounds);
-  };
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds)
+  }
 
-  let content = <StartGameScreen onStartGame={startGameHandler} />;
+  let content = <StartGameScreen onStartGame={startGameHandler} />
 
   if (userNumber && guessRounds <= 0) {
     content = (
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-    );
+    )
   } else if (guessRounds > 0) {
     content = (
       <GameOverScreen
@@ -36,7 +37,7 @@ export default function App() {
         userNumber={userNumber}
         onRestart={configureNewGameHandler}
       />
-    );
+    )
   }
 
   return (
@@ -44,11 +45,11 @@ export default function App() {
       <Header title="Guess a Number" />
       {content}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1
-  }
-});
+    flex: 1,
+  },
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import {
   View,
   Text,
@@ -6,66 +6,71 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert
-} from 'react-native';
+  Alert,
+} from 'react-native'
 
-import Card from '../components/Card';
-import Input from '../components/Input';
-import NumberContainer from '../components/NumberContainer';
-import Colors from '../constants/colors';
+import Card from '../components/Card'
+import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
+import MainButton from '../components/MainButton'
+import Colors from '../constants/colors'
 
-const StartGameScreen = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [confirmed, setConfirmed] = useState(false);
-  const [selectedNumber, setSelectedNumber] = useState();
+const StartGameScreen = (props) => {
+  const [enteredValue, setEnteredValue] = useState('')
+  const [confirmed, setConfirmed] = useState(false)
+  const [selectedNumber, setSelectedNumber] = useState()
 
-  const numberInputHandler = inputText => {
-    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
-  };
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+  }
 
   const resetInputHandler = () => {
-    setEnteredValue('');
-    setConfirmed(false);
-  };
+    setEnteredValue('')
+    setConfirmed(false)
+  }
 
   const confirmInputHandler = () => {
-    const chosenNumber = parseInt(enteredValue);
+    const chosenNumber = parseInt(enteredValue)
     if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
       Alert.alert(
         'Invalid number!',
         'Number has to be a number between 1 and 99.',
         [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
-      );
-      return;
+      )
+      return
     }
-    setConfirmed(true);
-    setSelectedNumber(chosenNumber);
-    setEnteredValue('');
-    Keyboard.dismiss();
-  };
+    setConfirmed(true)
+    setSelectedNumber(chosenNumber)
+    setEnteredValue('')
+    Keyboard.dismiss()
+  }
 
-  let confirmedOutput;
+  let confirmedOutput
 
   if (confirmed) {
     confirmedOutput = (
       <Card style={styles.summaryContainer}>
-        <Text>You selected</Text>
+        <BodyText>You selected</BodyText>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
+        <MainButton onPress={() => props.onStartGame(selectedNumber)}>
+          START GAME
+        </MainButton>
       </Card>
-    );
+    )
   }
 
   return (
     <TouchableWithoutFeedback
       onPress={() => {
-        Keyboard.dismiss();
+        Keyboard.dismiss()
       }}
     >
       <View style={styles.screen}>
-        <Text style={styles.title}>Start a New Game!</Text>
+        <TitleText style={styles.title}>Start a New Game!</TitleText>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
           <Input
             style={styles.input}
             blurOnSubmit
@@ -96,41 +101,41 @@ const StartGameScreen = props => {
         {confirmedOutput}
       </View>
     </TouchableWithoutFeedback>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   title: {
     fontSize: 20,
-    marginVertical: 10
+    marginVertical: 10,
   },
   inputContainer: {
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonContainer: {
     flexDirection: 'row',
     width: '100%',
     justifyContent: 'space-between',
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
   },
   button: {
-    width: 100
+    width: 100,
   },
   input: {
     width: 50,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   summaryContainer: {
     marginTop: 20,
-    alignItems: 'center'
-  }
-});
+    alignItems: 'center',
+  },
+})
 
-export default StartGameScreen;
+export default StartGameScreen
